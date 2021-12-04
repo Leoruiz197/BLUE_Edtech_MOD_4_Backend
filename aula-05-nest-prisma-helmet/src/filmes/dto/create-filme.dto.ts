ee7c1efbb91182b754b7bc872f9868e0b420af55
Leoruiz197 <ruizruizleo@hotmail>
2021-12-03 22:55:59 -0300

    helmet, segunda tabela

---
+++ aula-05-nest-prisma-helmet/src/filmes/dto/create-filme.dto.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsInt, IsString } from 'class-validator';
+
+export class CreateFilmeDto {
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  nome: string;
+
+  @IsNotEmpty()
+  @IsString()
+  capa: string;
+
+  @IsNotEmpty()
+  @IsString()
+  sinopse: string;
+}
