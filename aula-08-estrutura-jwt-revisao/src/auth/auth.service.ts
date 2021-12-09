481d6c8c1deccc61a09ffaaf1648e51e347563e7
Leoruiz197 <ruizruizleo@hotmail>
2021-12-08 22:58:11 -0300

    falta jwt

---
+++ aula-08-estrutura-jwt-revisao/src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly usuarioService: UsuariosService) {}
+
+  async login(loginUserDto: LoginDto) {
+    const user = await this.usuarioService.findByLogin(loginUserDto);
+
+    return;
+  }
+}
